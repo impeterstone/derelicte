1ca41585ba2e9bbf13e29b915835510b53f16029
Peter Shih <ptshih@gmail.com>
2011-08-11 00:37:07 -0700

    deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Derelicte::Application.config.secret_token = '7a08635a3efdadb70c603d99e6a569ad47c9669e212d94286e65d553cca5f83164f7ef0f8fb95cd74aed6869e386735f10c9cef8d44411d1fbc8fd5e17bee8ca'
